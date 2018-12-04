6fdcc06affe0634f86985711da9b132c2ccdb60a
Jacob Winkler <jwinkler1313@gmail.com>
2018-12-04 16:12:35 -0500

    quick

--- main.rb
+++ main.rb
@@ -1,11 +1,5 @@
-require_relative 'myGraph'
-require_relative 'myVertex'
-def file_in
-  if ARGV.length != 1
-    puts 'Too few arguments'
-    exit
-  end
-  file_name = ARGV[0]
+require_relative 'myNode'
+def file_in file_name
   if File.exist?(file_name)
     file = File.open(file_name, 'r')
   else
@@ -23,52 +17,92 @@ def parse_file file
 	  neigh = data[2].split(',')
 	  neigh_int = neigh.map(&:to_i)
 	  if neigh_int[0] == 0
-	    neigh_int = nil
+	    neigh_int = []
 	  end
-	  @vertices[data[0].to_i] = MyVertex.new(data[0].to_i,data[1],neigh_int)
+	  @nodes[data[0].to_i] = MyNode.new(data[0].to_i,data[1],neigh_int)
     else
-      @vertices[data[0].to_i] = MyVertex.new(data[0].to_i,data[1],nil)
+      @nodes[data[0].to_i] = MyNode.new(data[0].to_i,data[1],[])
     end
   end
 end
 
-def build_graph
-  for v in @vertices
-  if !v.nil?
-    @graph.add_vert v
-  end
+ def traverse(start, word)
+
+   if start.neighbors.length == 0
+     word += start.letter
+     @words.push word
+   else 
+     start.neighbors.each do |n|
+	   traverse(@nodes[n], word+start.letter)
+	 end
+ end
+ 
+ end
+ 
+def permutation(word, l, r)
+  if l == r
+    temp = word.join ''
+	temp = temp.downcase
+    @perms.push temp
+  else
+    (l..r).each do |i|
+	  word = swap word, l, i
+	  permutation word, l+1, r
+	  word = swap word, l, i
+	end
   end
 end
 
- def get_paths(start)
-   @vertex_list[vertex_count] = start
-   @vertex_count +=1
-   @graphs.vertices[start].visited = true
-   temp = @graph.vertices[start]
-   if !@graph.vertices[start].neigh.nil?
-    next_neigh = @graph.vertices[@graph.vertices[start].neigh[0]]
-	if next_neigh.visited
-	for  n in @graph.vertices[start].neigh
-	  if !@graph.vertices[n].visited 
-	    
-	  end
+def swap(word, n1, n2)
+  temp = word[n1]
+  word[n1]=word[n2]
+  word[n2]=temp
+  word
+end
+
+def get_dict file
+  file.each do |line|
+    @legal_words.push line.strip
+  end
+end
+
+def get_real_words
+  @perms.each do |n|
+    if @legal_words.include? n
+	  @real_words.push n
+	else
+	  #puts'stupid'
 	end
-   end
-   flag = 0;
-   while !temp.nil?
-     if @graph.vertices[temp.id].visited == 0
-	   flag
-	   get_paths(temp.id)
-	 end
-	 temp = @graph.vertices[temp.
-   end
- end
-@vertices = []
-@graph = MyGraph.new
-@vertex_list= []
-@vertex_count = 0
-file = file_in
+  end	
+end
+
+if ARGV.length != 1
+  puts 'Too few arguments'
+  exit
+end
+@nodes = []
+@words = []
+@perms = []
+@legal_words = []
+@real_words = []
+file = file_in ARGV[0]
 parse_file file
-build_graph
-puts @graph.get_paths(5)
-#puts @graph.edges.map {|x| x.join'|'}
+
+@nodes.each do |n|
+  if !n.nil?
+  traverse(n, '')
+  end
+  
+end
+
+(0..@words.length-1).each do |i|
+  permutation @words[i].chars,0, @words[i].length-1
+end
+dict_file = File.open('wordlist.txt','r')
+get_dict dict_file
+get_real_words
+longest_word = @real_words.max_by(&:length)
+puts "Longest word from #{ARGV[0]} graph is #{longest_word}"
+
+
+
--- myNode.rb
+++ myNode.rb
@@ -1,12 +1,10 @@
-class MyVertex
+class MyNode
   attr_accessor :id
   attr_accessor :letter
   attr_accessor :neighbors
-  attr_accessor : visited
   def initialize id, let, neighbors
     @id = id
 	@letter = let
 	@neighbors = neighbors
-	@visited = false
   end
 end
